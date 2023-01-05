aee42e640f07e042399295dae9178dc9dbc19e6d
VictorBK <victorbrian4@gmail.com>
2023-01-05 13:59:14 +0300

    Basic annotations-concat mended

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-#Type-annotated functon takes str1 and str2 as arguments
-#Function returns a concatenated string
+'''This module contains a concat function'''
+
 
 def concat(str1: str, str2: str) -> str:
-    #Concantenates two strings and return a string
+    '''Concantenates two strings and return a string'''
     return str1 + str2
