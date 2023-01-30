521c49238302101b697525c2bb6fbe1de394450b
VictorBK <victorbrian4@gmail.com>
2023-01-30 11:55:21 +0300

    Test client mend

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -21,26 +21,26 @@ from fixtures import TEST_PAYLOAD
 class TestGithubOrgClient(unittest.TestCase):
     """Tests the `GithubOrgClient` class."""
     @parameterized.expand([
-        ("google", {'login': "google"})
-        ("abc", {'login': "abc"})
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
     ])
     @patch(
         "client.get_json",
     )
     def test_org(self, org: str, resp: Dict, mocked_fxn: MagicMock) -> None:
         """Tests the `org` method."""
-        mocked_fxn.return_value = MagicMock(return_value = resp)
+        mocked_fxn.return_value = MagicMock(return_value=resp)
         gh_org_client = GithubOrgClient(org)
         self.assertEqual(gh_org_client.org(), resp)
-        mocked.fxn.assert_called_once_with(
-            "https.//api.github.com/orgs/{}".format(org)
+        mocked_fxn.assert_called_once_with(
+            "https://api.github.com/orgs/{}".format(org)
         )
 
     def test_public_repos_url(self) -> None:
         """Tests the `_public_repos_url` property."""
         with patch(
                 "client.GithubOrgClient.org",
-                new_callable = PropertyMock,
+                new_callable=PropertyMock,
                 ) as mock_org:
             mock_org.return_value = {
                 'repos_url': "https://api.github.com/users/google/repos",
@@ -54,7 +54,7 @@ class TestGithubOrgClient(unittest.TestCase):
     def test_public_repos(self, mock_get_json: MagicMock) -> None:
         """Tests the `public_repos` method."""
         test_payload = {
-            'repos_url: https://api.github.com/users/google/repos",
+            'repos_url': "https://api.github.com/users/google/repos",
             'repos': [
                 {
                     "id": 7697149,
@@ -125,7 +125,7 @@ class TestGithubOrgClient(unittest.TestCase):
         'apache2_repos': TEST_PAYLOAD[0][3],
     },
 ])
-class TestIntegrationsGithubOrgClient(unittest.TestCase):
+class TestIntegrationGithubOrgClient(unittest.TestCase):
     """Performs integration tests for the `GithubOrgClient` class."""
     @classmethod
     def setUpClass(cls) -> None:
